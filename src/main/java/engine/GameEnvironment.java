ae118e0cca88140941eb4a24a30ad2773407dbca
yuki <angelsflyinhell@namespace.media>
2022-11-07 12:40:57 +0100

    Update GameEnvironment.java

--- src/main/java/engine/GameEnvironment.java
+++ src/main/java/engine/GameEnvironment.java
@@ -56,7 +56,7 @@ public class GameEnvironment extends JPanel implements Runnable {
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        // set all components x, y
+        // set all components x, y or call game engine
 
         g2.dispose();
     }
