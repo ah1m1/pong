54f2326ee94d7db97375a98891fd88a66f0e72a6
yuki <angelsflyinhell@namespace.media>
2022-11-07 12:13:58 +0100

    Create Frame embedding

---
+++ src/main/java/engine/Embedding.java
@@ -0,0 +1,19 @@
+package engine;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Embedding {
+    public static void create(int size) {
+
+        JFrame frame = new JFrame();
+        frame.setSize((int) (size * 1.77777777778), size);
+        frame.setTitle("Ping by ah1m1/pong | angelsflyinhell.dev");
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        // add objects here
+
+        frame.setVisible(true);
+    }
+}
